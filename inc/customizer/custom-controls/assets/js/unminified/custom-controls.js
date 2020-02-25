afaa3d1c2f5157579a4617e36fae7699bf599f15
premanshu <premanshup@bsf.io>
2020-02-25 15:33:13 +0530

    fix: Clear button CSS added.

--- inc/customizer/custom-controls/assets/js/unminified/custom-controls.js
+++ inc/customizer/custom-controls/assets/js/unminified/custom-controls.js
@@ -771,27 +771,28 @@ wp.customize.controlConstructor['ast-responsive-background'] = wp.customize.Cont
 
 			responsive_input.parent( '.buttonset' ).find( '.switch-input' ).removeAttr('checked');
 			responsive_input.attr( 'checked', 'checked' );
-			
+
 			control.saveValue( screen, 'background-size', item_value );
 		});
 		
 		// Background-Position.
 		control.container.on( 'change', '.background-position select', function() {
 			var responsive_input = jQuery( this ),
-			screen = responsive_input.data( 'id' ),
-			item_value = responsive_input.val();
-			
+				screen = responsive_input.data( 'id' ),
+				item_value = responsive_input.val();
+
 			control.saveValue( screen, 'background-position', item_value );
 		});
 		
 		// Background-Attachment.
 		control.container.on( 'change click', '.background-attachment input', function() {
 			var responsive_input = jQuery( this ),
-			screen = responsive_input.data( 'id' ),
-			item_value = responsive_input.val();
-			
+				screen = responsive_input.data( 'id' ),
+				item_value = responsive_input.val();
+
 			responsive_input.parent( '.buttonset' ).find( '.switch-input' ).removeAttr('checked');
 			responsive_input.attr( 'checked', 'checked' );
+
 			control.saveValue( screen, 'background-attachment', item_value );
 		});
 		
