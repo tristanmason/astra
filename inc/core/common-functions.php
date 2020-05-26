c0319ed702ac106ac26fcfbf0d241adf26301169
imnavanath <navanathb@bsf.io>
2020-05-26 11:43:37 +0530

    Corrected convert hex color

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -43,13 +43,7 @@ if ( ! function_exists( 'astra_get_foreground_color' ) ) {
 			$rgba = preg_replace( '/[^0-9,]/', '', $hex );
 			$rgba = explode( ',', $rgba );
 
-			$r_val = (int) $rgba[0];
-			$g_val = (int) $rgba[1];
-			$b_val = (int) $rgba[2];
-
-			$rgba = ( ( $r_val * 0.299 ) + ( $g_val * 0.587 ) + ( $b_val * 0.114 ) );
-
-			return 186 < $rgba ? '#000000' : '#ffffff';
+			$hex = sprintf( '#%02x%02x%02x', $rgba[0], $rgba[1], $rgba[2] );
 		}
 
 		// Return if non hex.
