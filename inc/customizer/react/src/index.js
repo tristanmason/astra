7688fd63ea26f055dee76a14c78babba9e40e214
premanshup <premanshup@bsf.io>
2020-06-27 11:16:23 +0530

    test: Removed test control astra_text_control.

--- inc/customizer/react/src/index.js
+++ inc/customizer/react/src/index.js
@@ -1,11 +1,9 @@
 /* global wp */
 import { BaseControl } from './base/control.js';
-import { TextControl } from './text/control.js';
 import { HeadingControl } from './heading/control.js';
 import { HiddenControl } from './hidden/control.js';
 import { DescriptionControl } from './description/control.js';
 
-wp.customize.controlConstructor['astra_text_control'] = TextControl;
 wp.customize.controlConstructor['ast-heading'] = HeadingControl;
 wp.customize.controlConstructor['ast-hidden'] = HiddenControl;
 wp.customize.controlConstructor['ast-description'] = DescriptionControl;
