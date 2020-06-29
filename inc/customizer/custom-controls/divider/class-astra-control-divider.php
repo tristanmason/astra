eb8a3e33f22d2eaae393aa6fa46eb41230d62827
Abhishek <abhishekk@bsf.io>
2020-06-29 11:55:24 +0530

    Removed existing ast-divider template

--- inc/customizer/custom-controls/divider/class-astra-control-divider.php
+++ inc/customizer/custom-controls/divider/class-astra-control-divider.php
@@ -56,35 +56,4 @@ class Astra_Control_Divider extends WP_Customize_Control {
 		$this->json['separator']   = $this->separator;
 	}
 
-	/**
-	 * An Underscore (JS) template for this control's content (but not its container).
-	 *
-	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
-	 *
-	 * @see WP_Customize_Control::print_template()
-	 *
-	 * @access protected
-	 */
-	protected function content_template() {
-		?>
-
-		<# if ( data.caption ) { #>
-			<span class="customize-control-caption">{{{ data.caption }}}</span>
-		<# } #>
-
-		<# if ( data.separator ) { #>
-			<hr />
-		<# } #>
-
-		<label class="customizer-text">
-			<# if ( data.label ) { #>
-				<span class="customize-control-title">{{{ data.label }}}</span>
-			<# } #>
-			<# if ( data.description ) { #>
-				<span class="description customize-control-description">{{{ data.description }}}</span>
-			<# } #>
-		</label>
-		<?php
-	}
 }
