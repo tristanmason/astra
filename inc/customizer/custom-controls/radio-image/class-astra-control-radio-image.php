8df3a4e91d7cc735e08015ea3893048764ad7b29
RajkiranBagal <rajkiranb@bsf.io>
2020-07-03 11:29:38 +0530

    phpcs

--- inc/customizer/custom-controls/radio-image/class-astra-control-radio-image.php
+++ inc/customizer/custom-controls/radio-image/class-astra-control-radio-image.php
@@ -98,8 +98,8 @@ class Astra_Control_Radio_Image extends WP_Customize_Control {
 		}
 
 		$this->json['link'] = $this->get_link();
-		
-		$this->json['id']   = $this->id;
+
+		$this->json['id'] = $this->id;
 
 		$this->json['inputAttrs'] = '';
 		$this->json['labelStyle'] = '';
