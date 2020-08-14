8bba6091135ac92ecf3862b410a9cb4ce2a9fb91
premanshup <premanshup@bsf.io>
2020-08-14 14:04:31 +0530

    fix: NPM Run build minified.

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '36634d91c1053b17f42eb0c53122b5c4');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '2336bcfcc05b012de3d737615380fca3');
