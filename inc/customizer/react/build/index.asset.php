251e77cb74bf1da5b1814bab496232f5514b3aad
premanshup <premanshup@bsf.io>
2020-08-14 15:38:55 +0530

    fix: Merge conflict resolved.

--- inc/customizer/react/build/index.asset.php
+++ inc/customizer/react/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-polyfill'), 'version' => '7f32b8235bb1ba2e0e5ce1228813256b');
-=======
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'f313183671aa6dfbfe1e5ef9e97bcb62');
->>>>>>> 303f1e3b603f4d84e165de9fd36b196a0c655c08
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4059f7d427c5d20eb4a6e76781919dc6');
--- inc/customizer/react/src/index.js
+++ inc/customizer/react/src/index.js
@@ -2,9 +2,7 @@ import { BaseControl } from './base/control';
 import { headingControl } from './heading/control';
 import { hiddenControl } from './hidden/control';
 import { descriptionControl } from './description/control';
-<<<<<<< HEAD
 import { dividerControl } from './divider/control';
-=======
 import { settingsGroupControl } from './settings-group/control';
 import { colorControl } from './color/control.js';
 import { responsiveColorControl } from './responsive-color/control'
@@ -19,14 +17,11 @@ import { sliderControl } from './slider/control';
 import { radioImageControl } from './radio-image/control';
 import { responsiveSpacingControl }  from './responsive-spacing/control';
 import { selectControl } from './select/control';
->>>>>>> 303f1e3b603f4d84e165de9fd36b196a0c655c08
 
 wp.customize.controlConstructor['ast-heading'] = headingControl;
 wp.customize.controlConstructor['ast-hidden'] = hiddenControl;
 wp.customize.controlConstructor['ast-description'] = descriptionControl;
-<<<<<<< HEAD
 wp.customize.controlConstructor['ast-divider'] = dividerControl;
-=======
 wp.customize.controlConstructor['ast-settings-group'] = settingsGroupControl;
 wp.customize.controlConstructor['ast-color'] = colorControl;
 wp.customize.controlConstructor['ast-responsive-color'] = responsiveColorControl;
@@ -41,7 +36,6 @@ wp.customize.controlConstructor['ast-slider'] = sliderControl;
 wp.customize.controlConstructor['ast-radio-image'] = radioImageControl;
 wp.customize.controlConstructor['ast-responsive-spacing'] = responsiveSpacingControl;
 wp.customize.controlConstructor['ast-select'] = selectControl;
->>>>>>> 303f1e3b603f4d84e165de9fd36b196a0c655c08
 
 window.addEventListener( 'load', () => {
     console.log('Testing Console.');
