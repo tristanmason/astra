972524a2fa95ea99b8c2e01a4e3cd7b6d40438c5
imnavanath <navanathb@bsf.io>
2020-09-16 12:56:57 +0530

    fix: Dynamic breakpoint support added for Footer Social Icons preview JS

--- inc/customizer/builder/customizer/configuration/footer/social-icon/class-astra-footer-social-icons-component-loader.php
+++ inc/customizer/builder/customizer/configuration/footer/social-icon/class-astra-footer-social-icons-component-loader.php
@@ -75,6 +75,16 @@ class Astra_Footer_Social_Icons_Component_Loader {
 		$dir_name    = ( SCRIPT_DEBUG ) ? 'unminified' : 'minified';
 		$file_prefix = ( SCRIPT_DEBUG ) ? '' : '.min';
 		wp_enqueue_script( 'astra-footer-social-icons-customizer-preview-js', ASTRA_BUILDER_FOOTER_SOCIAL_ICONS_URI . '/assets/js/customizer-preview.js', array( 'customize-preview', 'astra-customizer-preview-js' ), ASTRA_THEME_VERSION, true );
+
+		// Localize variables for Astra Breakpoints JS.
+		wp_localize_script(
+			'astra-footer-social-icons-customizer-preview-js',
+			'astraBuilderPreview',
+			array(
+				'tablet_break_point' => astra_get_tablet_breakpoint(),
+				'mobile_break_point' => astra_get_mobile_breakpoint(),
+			)
+		);
 	}
 }
 
