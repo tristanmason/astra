0c08c59b7c964443747ca21a081c180551c41f76
imnavanath <navanathb@bsf.io>
2020-09-16 13:10:43 +0530

    chore: PHPCBF Run

--- inc/customizer/builder/customizer/configuration/header/menu/class-astra-header-menu-component-loader.php
+++ inc/customizer/builder/customizer/configuration/header/menu/class-astra-header-menu-component-loader.php
@@ -87,7 +87,7 @@ class Astra_Header_Menu_Component_Loader {
 			'astra-heading-menu-customizer-preview-js',
 			'AstraBuilderMenuData',
 			array(
-				'header_menu_count' => Astra_Constants::$num_of_header_menu,
+				'header_menu_count'  => Astra_Constants::$num_of_header_menu,
 				'tablet_break_point' => astra_get_tablet_breakpoint(),
 				'mobile_break_point' => astra_get_mobile_breakpoint(),
 			)
