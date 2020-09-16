859214df06238bfb49f2d842aa2f5da0587f3a00
RajkiranBagal <rajkiranb@bsf.io>
2020-09-16 12:46:45 +0530

    Added translation

--- inc/customizer/custom-controls/typography/class-astra-control-typography.php
+++ inc/customizer/custom-controls/typography/class-astra-control-typography.php
@@ -165,7 +165,8 @@ final class Astra_Control_Typography extends WP_Customize_Control {
 		?>
 		<input class="ast-font-variant-hidden-value" type="hidden" value="{{data.value}}">
 		</select>
+		<span class="ast-control-tooltip dashicons dashicons-editor-help ast-variant-description" title="<?php echo __( 'Only selected Font Variants will be loaded from Google Fonts.', 'astra' ); ?>"></span>
 		<?php 
-		echo '<span class="ast-control-tooltip dashicons dashicons-editor-help ast-variant-description" title="Only selected Font Variants will be loaded from Google Fonts."></span>';
+		
 	}
 }
