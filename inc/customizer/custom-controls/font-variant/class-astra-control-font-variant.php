947093aaf2433c70c509b87a3856e1061ee87417
RajkiranBagal <rajkiranb@bsf.io>
2020-09-17 11:14:21 +0530

    fix:added separate class file for font variant

---
+++ inc/customizer/custom-controls/font-variant/class-astra-control-font-variant.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Customizer Control: typography.
+ *
+ * @package     Astra
+ * @author      Astra
+ * @copyright   Copyright (c) 2020, Astra
+ * @link        https://wpastra.com/
+ * @since       1.0.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Typography control.
+ */
+final class Astra_Control_Font_Variant extends WP_Customize_Control {
+
+	/**
+	 * Used to connect controls to each other.
+	 *
+	 * @since 1.0.0
+	 * @var bool $connect
+	 */
+	public $connect = false;
+
+	/**
+	 * Option name.
+	 *
+	 * @since 1.0.0
+	 * @var string $name
+	 */
+	public $name = '';
+
+	/**
+	 * Option label.
+	 *
+	 * @since 1.0.0
+	 * @var string $label
+	 */
+	public $label = '';
+
+	/**
+	 * Option description.
+	 *
+	 * @since 1.0.0
+	 * @var string $description
+	 */
+	public $description = '';
+
+	/**
+	 * Control type.
+	 *
+	 * @since 1.0.0
+	 * @var string $type
+	 */
+	public $type = 'ast-font-variant';
+
+	/**
+	 * Used to connect variant controls to each other.
+	 *
+	 * @since 1.5.2
+	 * @var bool $variant
+	 */
+	public $variant = false;
+
+	/**
+	 * Used to set the mode for code controls.
+	 *
+	 * @since 1.0.0
+	 * @var bool $mode
+	 */
+	public $mode = 'html';
+
+	/**
+	 * Used to set the default font options.
+	 *
+	 * @since 1.0.8
+	 * @var string $ast_inherit
+	 */
+	public $ast_inherit = '';
+
+	/**
+	 * If true, the preview button for a control will be rendered.
+	 *
+	 * @since 1.0.0
+	 * @var bool $preview_button
+	 */
+	public $preview_button = false;
+
+	/**
+	 * Set the default font options.
+	 *
+	 * @since 1.0.8
+	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
+	 * @param string               $id      Control ID.
+	 * @param array                $args    Default parent's arguments.
+	 */
+	public function __construct( $manager, $id, $args = array() ) {
+		$this->ast_inherit = __( 'Inherit', 'astra' );
+		parent::__construct( $manager, $id, $args );
+	}
+
+	/**
+	 * Refresh the parameters passed to the JavaScript via JSON.
+	 *
+	 * @see WP_Customize_Control::to_json()
+	 */
+	public function to_json() {
+		
+		parent::to_json();
+
+		$this->json['label']               = esc_html( $this->label );
+		$this->json['description']         = $this->description;
+		$this->json['name']                = $this->name;
+		$this->json['value']               = $this->value();
+		$this->json['connect']             = $this->connect;
+		$this->json['variant']             = $this->variant;
+		$this->json['link']                = $this->get_link();
+		$this->json['ast_all_font_weight'] = $this->ast_all_font_weight;
+	}
+
+	/**
+	 * An Underscore (JS) template for this control's content (but not its container).
+	 *
+	 * Class variables for this control class are available in the `data` JS object;
+	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
+	 *
+	 * @see WP_Customize_Control::print_template()
+	 *
+	 * @access protected
+	 */
+	protected function content_template() {
+
+		?>
+
+		<label>
+		<# if ( data.label ) { #>
+			<span class="customize-control-title">{{{data.label}}}</span>
+		<# } #>
+		<# if ( data.description ) { #>
+			<span class="description customize-control-description">{{{data.description}}}</span>
+		<# } #>
+
+		</label>
+		<select data-inherit="<?php echo esc_attr( $this->ast_inherit ); ?>" <?php $this->link(); ?>  multiple data-name={{ data.name }}
+		data-value="{{data.value}}" 
+
+		<# if ( data.connect ) { #>
+			data-connected-control={{ data.connect }}
+		<# } #>
+		<# if ( data.variant ) { #>
+			data-connected-variant="{{data.variant}}"; 
+		<# } #>
+
+		>
+		<?php 
+			$values = explode( ',', $this->value() );
+		foreach ( $values as $key => $value ) {
+			echo '<option value="' . esc_attr( $value ) . '" selected="selected" >' . esc_html( $value ) . '</option>';
+		}
+		?>
+		<input class="ast-font-variant-hidden-value" type="hidden" value="{{data.value}}">
+		</select>
+		<span class="ast-control-tooltip dashicons dashicons-editor-help ast-variant-description" title="<?php esc_html_e( 'Only selected Font Variants will be loaded from Google Fonts.', 'astra' ); ?>"></span>
+		<?php 
+		
+	}
+}
