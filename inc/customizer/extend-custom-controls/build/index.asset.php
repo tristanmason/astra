fdb97d728449f2eb15321ca4eff4e0b5a585b761
premanshup <premanshup@bsf.io>
2020-09-18 13:24:18 +0530

    improvement: Updated clear button disabled CSS.

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '749d6038436b7a6f2c152963dd6d8b76');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '0543a57d2bbb30839a3fbd0072e6eaf3');
