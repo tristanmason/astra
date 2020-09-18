650a2ed7833d8f7015897543055f388f262ef524
Vrunda <vrundak@bsf.io>
2020-09-18 18:08:19 +0530

    fix: Transparent page meta property not being applied for Above Header issue fixed.

--- inc/core/builder/class-astra-builder-options.php
+++ inc/core/builder/class-astra-builder-options.php
@@ -137,7 +137,7 @@ function astra_hf_builder_customizer_defaults( $defaults ) {
 	$defaults['hba-header-bottom-border-color']     = '#eaeaea';
 	$defaults['hba-header-bg-obj-responsive']       = array(
 		'desktop' => array(
-			'background-color'      => '#ffffff',
+			'background-color'      => '',
 			'background-image'      => '',
 			'background-repeat'     => 'repeat',
 			'background-position'   => 'center center',
