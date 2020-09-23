40e31b8598e01f1ecd001a3fb0ea2b0ed916ed9d
premanshup <premanshup@bsf.io>
2020-09-23 14:49:33 +0530

    fix: Content - Blog - Archive - Group Control not working fixed.

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '3c105c36aa455db4561ed261f67b0486');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '9d6bafe475a79c9c837261d324092397');
