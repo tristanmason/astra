bd5c0fdd7c239c03e10037301ef7cdb9541e92d7
Rohit Patil <rohitp@bsf.io>
2020-09-25 12:55:14 +0530

    fix: Setting icon issues

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'bc317ecf453df31a4380aca3047d4fc6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4b04f1e04683d19e4be86e69641d7834');
