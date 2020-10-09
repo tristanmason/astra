0719675b8c0e7f2564fd0b8886852ba91e011a78
premanshup <premanshup@bsf.io>
2020-10-09 11:20:42 +0530

    fix: By default keep the links interconnected.

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '980a591a7f1ffa8599b74f4e01deb035');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'ec2e41074023eeccda17cc621c0f2c55');
