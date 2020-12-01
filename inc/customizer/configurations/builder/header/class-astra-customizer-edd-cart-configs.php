16efd12dedafd50f549a50d93d88cca41a23c751
sagarb <sagarb@bsf.io>
2020-12-01 21:09:19 +0530

    resolved Woo and EDD cart issues

--- inc/customizer/configurations/builder/header/class-astra-customizer-edd-cart-configs.php
+++ inc/customizer/configurations/builder/header/class-astra-customizer-edd-cart-configs.php
@@ -53,8 +53,6 @@ class Astra_Customizer_Edd_Cart_Configs extends Astra_Customizer_Config_Base {
 				'panel'    => 'panel-header-builder-group',
 			),
 
-			
-
 			/**
 			 * Option: Header cart total
 			 */
@@ -152,6 +150,19 @@ class Astra_Customizer_Edd_Cart_Configs extends Astra_Customizer_Config_Base {
 			),
 			
 		);
+		
+		if ( Astra_Builder_Helper::$is_header_footer_builder_active ) {
+			$_edd_configs = array(
+				'name'        => ASTRA_THEME_SETTINGS . '[builder-header-edd-cart-tabs]',
+				'section'     => $_section,
+				'type'        => 'control',
+				'control'     => 'ast-builder-header-control',
+				'priority'    => 0,
+				'description' => '',
+			);
+
+			$configurations = array_merge( $configurations, $_edd_configs );
+		}
 
 		return array_merge( $configurations, $_configs );
 	}
