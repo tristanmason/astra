adb771595665fe8c8abb7b4b76b97e556ee2f2b1
RajkiranBagal <rajkiranb@bsf.io>
2020-12-03 16:28:46 +0530

    fix: Menu - Stack on mobile - Doesn't work in the customizer preview

--- inc/builder/type/header/menu/assets/js/customizer-preview.js
+++ inc/builder/type/header/menu/assets/js/customizer-preview.js
@@ -278,7 +278,7 @@
 				wp.customize( 'astra-settings[header-menu'+ index +'-menu-stack-on-mobile]', function( setting ) {
 					setting.bind( function( stack ) {
 
-						var menu_div = jQuery( '#ast-mobile-header #ast-hf-menu-'+ index  );
+						var menu_div = jQuery( '#ast-mobile-header #ast-hf-menu-'+ index + ' .main-header-menu'  );
 						menu_div.removeClass('inline-on-mobile');
 						menu_div.removeClass('stack-on-mobile');
 
