18e8b65ad1ed3c23cdad8ef4ec78b1c3727581ed
sagarb <sagarb@bsf.io>
2020-12-11 17:06:24 +0530

    fix: removed breadcrum condition

--- inc/addons/breadcrumbs/dynamic-css/dynamic.css.php
+++ inc/addons/breadcrumbs/dynamic-css/dynamic.css.php
@@ -26,11 +26,7 @@ add_filter( 'astra_dynamic_theme_css', 'astra_breadcrumb_section_dynamic_css' );
 function astra_breadcrumb_section_dynamic_css( $dynamic_css, $dynamic_css_filtered = '' ) {
 
 	$breadcrumb_position = astra_get_option( 'breadcrumb-position', 'none' );
-		
-	if ( 'none' === $breadcrumb_position ) {
-		return $dynamic_css;
-	}
-
+	
 	$dynamic_css .= astra_parse_css(
 		array(
 			'.ast-breadcrumbs .trail-browse, .ast-breadcrumbs .trail-items, .ast-breadcrumbs .trail-items li' => array(
@@ -62,6 +58,10 @@ function astra_breadcrumb_section_dynamic_css( $dynamic_css, $dynamic_css_filter
 		''
 	);
 
+	if ( 'none' === $breadcrumb_position ) {
+		return $dynamic_css;
+	}
+	
 	/**
 	 * Set CSS Params
 	 */
