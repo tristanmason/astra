86c7654fe8cca79328cf68193a42ba6bf0223810
sushmak02 <sushmak@bsf.io>
2020-12-14 18:14:03 +0530

    fix: CSS not applying at frontend issue fixed

--- inc/builder/type/header/account/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/account/dynamic-css/dynamic.css.php
@@ -26,7 +26,7 @@ add_filter( 'astra_dynamic_theme_css', 'astra_hb_account_dynamic_css' );
  */
 function astra_hb_account_dynamic_css( $dynamic_css, $dynamic_css_filtered = '' ) {
 
-	if ( ! Astra_Builder_Helper::is_component_loaded( 'header', 'account' ) ) {
+	if ( ! Astra_Builder_Helper::is_component_loaded( 'account', 'header' ) ) {
 		return $dynamic_css;
 	}
 
