2b018404a42e14b3a42fdb5be2f962b5f5a43a37
Vrunda <vrundak@bsf.io>
2020-12-16 14:19:59 +0530

    chore: npm run build

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '10839862e4474a9f28a383429cd2fa96');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '5ad9eac0887c9a9a613261b1e317e3ca');
