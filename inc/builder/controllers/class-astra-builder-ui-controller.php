2b47d87c8c2d803108070b3223397f2efd6901c3
sagarb <sagarb@bsf.io>
2021-01-11 10:28:44 +0530

    chore: optimize site-brancding div of mobile header.

--- inc/builder/controllers/class-astra-builder-ui-controller.php
+++ inc/builder/controllers/class-astra-builder-ui-controller.php
@@ -238,7 +238,6 @@ if ( ! class_exists( 'Astra_Builder_UI_Controller' ) ) {
 		 */
 		public static function render_site_identity() {
 			?>
-			<div class="site-branding">
 				<?php
 				if ( is_customize_preview() ) {
 					self::render_customizer_edit_button();
@@ -249,14 +248,13 @@ if ( ! class_exists( 'Astra_Builder_UI_Controller' ) ) {
 					echo astra_attr(
 						'site-identity',
 						array(
-							'class' => 'ast-site-identity',
+							'class' => 'site-branding ast-site-identity',
 						)
 					);
 				?>
 				>
 					<?php astra_logo(); ?>
 				</div>
-			</div>
 			<!-- .site-branding -->
 			<?php
 		}
