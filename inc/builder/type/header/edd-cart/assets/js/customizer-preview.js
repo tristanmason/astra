acb9b06bf5d0b7c57461c6a5afddd2b5bbe81631
imnavanath <navanathb@bsf.io>
2021-01-11 17:27:57 +0530

    fix: Preview EDD Bg tray color not working

--- inc/builder/type/header/edd-cart/assets/js/customizer-preview.js
+++ inc/builder/type/header/edd-cart/assets/js/customizer-preview.js
@@ -32,11 +32,11 @@
 	astra_css( 'astra-settings[header-edd-cart-link-color]', 'color', selector + ' .widget_edd_cart_widget a, ' + selector + ' .widget_edd_cart_widget a.edd-remove-from-cart, ' + selector + ' .widget_edd_cart_widget .cart-total, ' + selector + ' .widget_edd_cart_widget a.edd-remove-from-cart:after' );
 	astra_css( 'astra-settings[header-edd-cart-link-color]', 'border-color', selector + ' .widget_edd_cart_widget a.edd-remove-from-cart:after' );
 
-	astra_css( 'astra-settings[header-edd-cart-background-color]', 'background-color', selector + ' .widget_edd_cart_widget' );
-	astra_css( 'astra-settings[header-edd-cart-background-color]', 'border-color', selector + ' .widget_edd_cart_widget' );
-	astra_css( 'astra-settings[header-edd-cart-background-color]', 'border-bottom-color', selector + ' .widget_edd_cart_widget:before, ' + selector + ' .widget_edd_cart_widget:after' );
+	astra_css( 'astra-settings[header-edd-cart-background-color]', 'background-color', '.ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget' );
+	astra_css( 'astra-settings[header-edd-cart-background-color]', 'border-color', '.ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget' );
+	astra_css( 'astra-settings[header-edd-cart-background-color]', 'border-bottom-color', '.ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget:before, .ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget:after' );
 
-	astra_css( 'astra-settings[header-edd-cart-separator-color]', 'border-bottom-color', selector + ' .widget_edd_cart_widget .edd-cart-item, ' + selector + ' .widget_edd_cart_widget .edd-cart-number-of-items, ' + selector + ' .widget_edd_cart_widget .edd-cart-meta' );
+	astra_css( 'astra-settings[header-edd-cart-separator-color]', 'border-bottom-color', '.ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget .edd-cart-item, .ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget .edd-cart-number-of-items, .ast-builder-layout-element ' + selector + ' .widget_edd_cart_widget .edd-cart-meta' );
 
 	astra_css( 'astra-settings[header-edd-checkout-btn-text-color]', 'color', '.ast-edd-site-header-cart .widget_edd_cart_widget .edd_checkout a, .widget_edd_cart_widget .edd_checkout a' );
 	astra_css( 'astra-settings[header-edd-checkout-btn-background-color]', 'border-color', '.ast-edd-site-header-cart .widget_edd_cart_widget .edd_checkout a, .widget_edd_cart_widget .edd_checkout a' );
