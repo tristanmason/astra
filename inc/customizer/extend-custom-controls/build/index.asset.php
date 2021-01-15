11860f9e571d5afffdb34c91ac19b31f79927475
RajkiranBagal <rajkiranb@bsf.io>
2021-01-15 15:43:59 +0530

    fix: Duplicate icon elements in Social Icons component.

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '472aaec8cc96460baeebf32ec81b5054');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '251243e3bca34215f9edaad13061c098');
