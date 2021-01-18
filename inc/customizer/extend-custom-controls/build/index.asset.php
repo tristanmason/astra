ee0806fe2362dc9c74c5535e91848e042507039f
ShubhamW <shubhamw@bsf.io>
2021-01-18 16:25:13 +0530

    chore: added a flag for responsive control

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a4a24479cefffedf4fc275f17e64f13d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a46f361adc1e081f77584e56d8f2dbd3');
--- inc/customizer/extend-custom-controls/src/responsive/responsive-component.js
+++ inc/customizer/extend-custom-controls/src/responsive/responsive-component.js
@@ -80,12 +80,12 @@ const ResponsiveComponent = props => {
 	let descriptionHtml = null;
 	let inputHtml = null;
 
-	responsive = ( false === responsive ) ? false : true ;
+	let responsive_flag = ( false === responsive ) ? false : true ;
 
 	if (label) {
 		labelHtml = <span className="customize-control-title">{label}</span>;
 
-		if (responsive) {
+		if (responsive_flag) {
 			responsiveHtml = <ul key={'ast-resp-ul'} className="ast-responsive-btns">
 				<li key={'desktop'} className="desktop active">
 					<button type="button" className="preview-desktop" data-device="desktop">
@@ -110,7 +110,7 @@ const ResponsiveComponent = props => {
 		descriptionHtml = <span className="description customize-control-description">{description}</span>;
 	}
 
-	if (responsive) {
+	if (responsive_flag) {
 		inputHtml = <>
 			{renderInputHtml('desktop', 'active')}
 			{renderInputHtml('tablet')}
