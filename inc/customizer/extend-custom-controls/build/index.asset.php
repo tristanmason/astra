8ac5e191649c14dcb5f3509145980ce9c846227d
Rohit Patil <rohitp@bsf.io>
2021-02-01 11:03:43 +0530

    fix: Randon click near responsive slider changing the context

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '0488065202da365c8071bd414776e64b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4af3d9df8a5fb4c688a29a8cc79eba48');
