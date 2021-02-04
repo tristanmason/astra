b6fadd69c3bc8587eb8c1a8cd1d7a82e436a9332
sagarb <sagarb@bsf.io>
2021-02-04 19:03:21 +0530

    build: PHPCS

--- inc/builder/type/header/above-header/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/above-header/dynamic-css/dynamic.css.php
@@ -117,7 +117,7 @@ function astra_above_header_row_setting( $dynamic_css, $dynamic_css_filtered = '
 
 	$dynamic_css .= Astra_Builder_Base_Dynamic_CSS::prepare_advanced_margin_padding_css( $_section, $parent_selector );
 
-	$dynamic_css .= Astra_Builder_Base_Dynamic_CSS::prepare_visibility_css( $_section, '.ast-above-header-bar', 'block', $mobile_tablet_default_display = 'grid' );	
+	$dynamic_css .= Astra_Builder_Base_Dynamic_CSS::prepare_visibility_css( $_section, '.ast-above-header-bar', 'block', $mobile_tablet_default_display = 'grid' ); 
 
 	return $dynamic_css;
 }
