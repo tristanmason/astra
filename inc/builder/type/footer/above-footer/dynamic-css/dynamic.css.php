64048db4323edfe691a87c0088c9ddb0ccadbac5
Sushma Kure <sushmak@bsf.io>
2021-02-08 16:56:24 +0530

    fix: footer sections related CSS removed

--- inc/builder/type/footer/above-footer/dynamic-css/dynamic.css.php
+++ inc/builder/type/footer/above-footer/dynamic-css/dynamic.css.php
@@ -49,12 +49,11 @@ function astra_fb_above_footer_dynamic_css( $dynamic_css, $dynamic_css_filtered 
 	$css_output_desktop = array(
 
 		$selector => astra_get_responsive_background_obj( $footer_bg, 'desktop' ),
-		$selector . ' .ast-builder-grid-row, ' . $selector . ' .ast-builder-grid-row .site-footer-section' => array(
+		$selector . ' .ast-builder-grid-row' => array(
 			'grid-column-gap' => astra_get_css_value( $inner_spacing_desktop, 'px' ),
 		),
 		$selector . ' .ast-builder-grid-row, ' . $selector . ' .ast-builder-grid-row .site-footer-section' => array(
 			'align-items'     => astra_get_option( 'hba-footer-vertical-alignment' ),
-			'grid-column-gap' => astra_get_css_value( $inner_spacing_desktop, 'px' ),
 		),
 		$selector . '.ast-footer-row-inline .site-footer-section' => array(
 			'display'       => 'flex',
