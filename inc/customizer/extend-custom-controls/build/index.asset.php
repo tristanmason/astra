906bbeb5dc0b3033fe406035d25d32f602544213
RajkiranBagal <rajkiranb@bsf.io>
2021-02-09 16:12:40 +0530

    fix: Updated the Icons Markup & CSS

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '6f3ec1386f6ec280b1ecd121967d26bb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '9b7c8b0ccd083d0b5cb699571a978ce4');
