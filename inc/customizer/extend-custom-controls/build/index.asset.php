4cc290d38cd510d78a7bbbec161ae714b9990c27
imnavanath <navanathb@bsf.io>
2021-02-10 16:35:10 +0530

    refactor: Removed dashicons from General & Design tab in customizer

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '6f3ec1386f6ec280b1ecd121967d26bb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '013b6e42d7372420ffbd384bb3ddf923');
