bbde5e5900398c45edd39ef06d37599583d781a4
sagarb <sagarb@bsf.io>
2021-02-15 14:41:53 +0530

    refactor: removed header class and footer duplicate visibility class

--- template-parts/header/builder/header-row.php
+++ template-parts/header/builder/header-row.php
@@ -21,7 +21,7 @@ if ( Astra_Builder_Helper::is_row_empty( $row, 'header', 'desktop' ) ) {
 
 	?>
 	<div class="ast-<?php echo esc_attr( $row_label ); ?>-header-wrap <?php echo 'primary' === $row ? 'main-header-bar-wrap' : ''; ?> ">
-		<div class="<?php echo esc_attr( 'ast-' . $row . '-header-bar ast-' . $row . '-header' ); ?> <?php echo 'primary' === $row ? 'main-header-bar' : ''; ?> ast-builder-grid-row-container site-header-focus-item" data-section="<?php echo esc_attr( $customizer_editor_row ); ?>">
+		<div class="<?php echo esc_attr( 'ast-' . $row . '-header-bar ast-' . $row . '-header' ); ?> <?php echo 'primary' === $row ? 'main-header-bar' : ''; ?> site-header-focus-item" data-section="<?php echo esc_attr( $customizer_editor_row ); ?>">
 			<?php
 			if ( is_customize_preview() ) {
 				Astra_Builder_UI_Controller::render_grid_row_customizer_edit_button( 'Header', $row );
