a940396e925060563abadf3654512f1f0f224bcf
Vrunda <vrundak@bsf.io>
2021-02-19 18:25:45 +0530

    fix: Added new labels for extra phone and email

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '6df84cc57890eb3d5675639908bc2d7e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4498d41d41144bebd879fd73940b46a7');
