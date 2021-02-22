d51661e5822d75a8d736730b3f2fcda90b83d9d0
Vrunda <vrundak@bsf.io>
2021-02-22 15:04:53 +0530

    fix: Invisible icons in customizer issue fixed

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'b60f7275439c06d49f778157536d7857');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'b2d2594e83b27350cd9f4cd16d825b78');
