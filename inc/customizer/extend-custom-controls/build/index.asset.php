56cbbde653da2d8014b38ff950b694f8783770dc
Abhishek <abhishekk@bsf.io>
2021-02-24 17:27:45 +0530

    fix: Sonarlint unnecessary semicolon issue

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '2e38867c1949221ba68c8e954bf606b4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'e672051151b058cbe026f9d46513cad7');
