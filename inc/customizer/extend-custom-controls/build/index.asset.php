5b0a85c25b8c1efb5ddabb96054deb3893a5fb30
imnavanath <navanathb@bsf.io>
2021-03-01 11:03:58 +0530

    refactor: Added components are unused saying message in builder panel when builder layout is empty

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '3df547473a3ee7693dc127176644d405');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '7d49a3abfbd5b5a95ba9ca1275f10e02');
