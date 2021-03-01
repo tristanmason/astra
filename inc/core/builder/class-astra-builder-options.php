66338e95f27a5077ed703956787e7d40fc4cd0c3
imnavanath <navanathb@bsf.io>
2021-03-01 14:22:57 +0530

    feat: Added woo cart icon color option for transparent header

--- inc/core/builder/class-astra-builder-options.php
+++ inc/core/builder/class-astra-builder-options.php
@@ -882,6 +882,7 @@ function astra_hf_builder_customizer_defaults( $defaults ) {
 	 */
 	$defaults['woo-header-cart-icon-style']    = 'none';
 	$defaults['header-woo-cart-icon-color']    = '';
+	$defaults['transparent-header-woo-cart-icon-color']    = '';
 	$defaults['woo-header-cart-icon-radius']   = 3;
 	$defaults['woo-header-cart-total-display'] = true;
 	$defaults['woo-header-cart-title-display'] = true;
