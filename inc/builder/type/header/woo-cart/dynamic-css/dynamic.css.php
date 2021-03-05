60b5350d6599220e2de1f085af41f2dd97cf77b7
imnavanath <navanathb@bsf.io>
2021-03-05 12:59:37 +0530

    chore: PHPCBF Run

--- inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
@@ -364,8 +364,8 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 			),
 			// Outline icon colors.
 			'.ast-menu-cart-outline .ast-cart-menu-wrap .count, .ast-menu-cart-outline .ast-addon-cart-wrap' => array(
-				'border'     => '2px solid ' . $icon_color,
-				'color'      => esc_attr( $icon_color ),
+				'border' => '2px solid ' . $icon_color,
+				'color'  => esc_attr( $icon_color ),
 			),
 			// Outline Info colors.
 			$selector . ' .ast-menu-cart-outline .ast-woo-header-cart-info-wrap' => array(
@@ -389,8 +389,8 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 			),
 			// Outline icon colors.
 			'.ast-theme-transparent-header .ast-menu-cart-outline .ast-cart-menu-wrap .count, .ast-theme-transparent-header .ast-menu-cart-outline .ast-addon-cart-wrap' => array(
-				'border'     => '2px solid ' . $transparent_header_icon_color,
-				'color'      => esc_attr( $transparent_header_icon_color ),
+				'border' => '2px solid ' . $transparent_header_icon_color,
+				'color'  => esc_attr( $transparent_header_icon_color ),
 			),
 			// Outline Info colors.
 			$trans_header_selector . ' .ast-menu-cart-outline .ast-woo-header-cart-info-wrap' => array(
