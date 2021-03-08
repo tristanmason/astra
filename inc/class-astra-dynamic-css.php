e77f73c29a6ec86a7ab1bb9f4dab5a6df1159db3
premanshup <premanshup@bsf.io>
2021-03-08 16:25:09 +0530

    fix: Mobile - 1. Subemenu arrow direction after closing downwards fixed. 2. Extra transition on arrows on submenu toggle.

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -2778,6 +2778,9 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 						'margin-left'             => '10px',
 						'line-height'             => 'normal',
 					),
+					'.ast-mobile-popup-drawer .main-header-bar-navigation .ast-submenu-expanded>.ast-menu-toggle::before'=> array(
+						'transform'                 => 'rotateX(180deg)',
+					),
 				);
 			} else {
 				if ( ! Astra_Builder_Helper::$is_header_footer_builder_active ) {
@@ -2804,6 +2807,9 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 							'margin-left' => '0px',
 							'width' => '.65em',
 							'transform'     => 'translate(0, -2px) rotateZ(270deg)',
+						),
+						'.ast-mobile-popup-content .ast-submenu-expanded > .ast-menu-toggle' => array(
+							'transform'                 => 'rotateX(180deg)',
 						),
 					);
 				}
