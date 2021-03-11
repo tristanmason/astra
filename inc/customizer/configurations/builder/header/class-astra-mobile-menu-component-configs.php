2ae5319b3b8109e7b431657c06ee7823cde1fa30
RajkiranBagal <rajkiranb@bsf.io>
2021-03-11 17:47:54 +0530

    Renamed mobile menu to Off-Canavs Menu

--- inc/customizer/configurations/builder/header/class-astra-mobile-menu-component-configs.php
+++ inc/customizer/configurations/builder/header/class-astra-mobile-menu-component-configs.php
@@ -53,7 +53,7 @@ if ( class_exists( 'Astra_Customizer_Config_Base' ) ) {
 				array(
 					'name'     => $_section,
 					'type'     => 'section',
-					'title'    => __( 'Mobile Menu', 'astra' ),
+					'title'    => __( 'Off-Canvas Menu', 'astra' ),
 					'panel'    => 'panel-header-builder-group',
 					'priority' => 40,
 				),
