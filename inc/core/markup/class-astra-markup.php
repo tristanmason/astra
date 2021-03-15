31178ae309d2fe42213bd03c66d80a3c865aa8bf
sagarb <sagarb@bsf.io>
2021-03-15 12:37:53 +0530

    fix: siderbar brakpoint at 921

--- inc/core/markup/class-astra-markup.php
+++ inc/core/markup/class-astra-markup.php
@@ -169,7 +169,7 @@ class Astra_Markup {
 	 * @return string.
 	 */
 	public function ast_blog_common_css() {
-		return Astra_Builder_Helper::apply_flex_based_css() ? 'ast-grid-common-col' : 'ast-col-sm-12'; 
+		return Astra_Builder_Helper::apply_flex_based_css() ? 'ast-grid-common-col ast-full-width' : 'ast-col-sm-12'; 
 	}
 
 	/**
