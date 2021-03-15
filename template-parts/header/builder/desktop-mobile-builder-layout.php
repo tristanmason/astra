98b6a52bc077d22d3f92c2669e6f5bc7c064a8a3
RajkiranBagal <rajkiranb@bsf.io>
2021-03-15 18:45:05 +0530

    phpcs

--- template-parts/header/builder/desktop-mobile-builder-layout.php
+++ template-parts/header/builder/desktop-mobile-builder-layout.php
@@ -35,10 +35,10 @@ if ( 'full-width' === $mobile_header_type ) {
 
 		astra_main_header_bar_bottom();
 
-if ( 'dropdown' === astra_get_option( 'mobile-header-type' ) || is_customize_preview() ) {
-$content_alignment = astra_get_option( 'header-offcanvas-content-alignment', 'flex-start' );
-$alignment_class   = 'content-align-' . $content_alignment . ' ';
-?>
+	if ( 'dropdown' === astra_get_option( 'mobile-header-type' ) || is_customize_preview() ) {
+		$content_alignment = astra_get_option( 'header-offcanvas-content-alignment', 'flex-start' );
+		$alignment_class   = 'content-align-' . $content_alignment . ' ';
+		?>
 <div class="ast-mobile-header-content <?php echo esc_attr( $alignment_class ); ?>">
 	<div class="ast-builder-menu-1 ast-builder-menu ast-flex ast-builder-menu-1-focus-item ast-builder-layout-element site-header-focus-item" data-section="section-hb-menu-1">
 		<?php do_action( 'astra_header_menu_1' ); ?>
