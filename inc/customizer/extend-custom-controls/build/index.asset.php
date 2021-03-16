0d17d5d61d68e665ffb60cc6733516c8795c69d0
imnavanath <navanathb@bsf.io>
2021-03-16 15:36:34 +0530

    fix: Stack fullwidth 2 column layout SVG is missing in customizer footer column layouts

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '076fa14a22872c600b8306a4f97b59f1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '273e3043477e965c3b912b066b254a4b');
