28bde95527bd3aab4159fc10e9f8db9107072a5c
RajkiranBagal <rajkiranb@bsf.io>
2021-03-25 14:14:06 +0530

    fix: Menu Hover color not working on Frontend.

--- inc/builder/type/header/mobile-menu/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/mobile-menu/dynamic-css/dynamic.css.php
@@ -26,7 +26,7 @@ add_filter( 'astra_dynamic_theme_css', 'astra_hb_mobile_menu_dynamic_css', 11 );
  */
 function astra_hb_mobile_menu_dynamic_css( $dynamic_css, $dynamic_css_filtered = '' ) {
 
-	if ( ! Astra_Builder_Helper::is_component_loaded( 'mobile-menu', 'header', 'mobile' ) ) {
+	if ( ! Astra_Builder_Helper::is_component_loaded( 'mobile-menu', 'header' ) ) {
 		return $dynamic_css;
 	}
 
@@ -47,7 +47,7 @@ function astra_hb_mobile_menu_dynamic_css( $dynamic_css, $dynamic_css_filtered =
 	$menu_resp_bg_color_hover  = astra_get_option( 'header-mobile-menu-h-bg-color-responsive' );
 	$menu_resp_color_active    = astra_get_option( 'header-mobile-menu-a-color-responsive' );
 	$menu_resp_bg_color_active = astra_get_option( 'header-mobile-menu-a-bg-color-responsive' );
-
+	
 	$menu_resp_color_desktop = ( isset( $menu_resp_color['desktop'] ) ) ? $menu_resp_color['desktop'] : '';
 	$menu_resp_color_tablet  = ( isset( $menu_resp_color['tablet'] ) ) ? $menu_resp_color['tablet'] : '';
 	$menu_resp_color_mobile  = ( isset( $menu_resp_color['mobile'] ) ) ? $menu_resp_color['mobile'] : '';
@@ -118,7 +118,7 @@ function astra_hb_mobile_menu_dynamic_css( $dynamic_css, $dynamic_css_filtered =
 		$selector                                          => array(
 			'font-size' => astra_get_font_css_value( $menu_font_size_desktop, $menu_font_size_desktop_unit ),
 		),
-		'.ast-hfb-header ' . $selector . ' .main-header-menu .menu-item > .menu-link' => array(
+		$selector . ' .main-header-menu .menu-item > .menu-link' => array(
 			'color'          => $menu_resp_color_desktop,
 			'padding-top'    => astra_responsive_spacing( $menu_spacing, 'top', 'desktop' ),
 			'padding-bottom' => astra_responsive_spacing( $menu_spacing, 'bottom', 'desktop' ),
