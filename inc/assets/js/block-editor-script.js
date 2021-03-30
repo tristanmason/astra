fb33db63c3f9aab30e57fb79121b85c2e4a22c02
imnavanath <navanathb@bsf.io>
2021-03-30 18:38:57 +0530

    chore: Resolved PR comments for JS file spacings

--- inc/assets/js/block-editor-script.js
+++ inc/assets/js/block-editor-script.js
@@ -12,47 +12,32 @@ function astra_onload_function() {
 
 			document.querySelector('#site-content-layout').addEventListener('change',function( event ) {
 
-				var bodyClass = document.querySelector('body');
-
-				var contentLayout = document.getElementById('site-content-layout').value;
-
+				var bodyClass = document.querySelector('body'),
+					contentLayout = document.getElementById('site-content-layout').value;
 				if ( 'content-boxed-container' == contentLayout ) {
-
 					bodyClass.classList.add('ast-separate-container');
 					bodyClass.classList.remove('ast-two-container' , 'ast-page-builder-template' , 'ast-plain-container');
-
 				} else if ( 'boxed-container' == contentLayout ) {
-
 					bodyClass.classList.add('ast-separate-container' , 'ast-two-container');
 					bodyClass.classList.remove('ast-page-builder-template' , 'ast-plain-container');
-
 				} else if ( 'page-builder' == contentLayout ) {
-
 					bodyClass.classList.add('ast-page-builder-template');
 					bodyClass.classList.remove('ast-two-container' , 'ast-plain-container' , 'ast-separate-container');
-
 				} else if ( 'plain-container' == contentLayout ) {
-
 					bodyClass.classList.add('ast-plain-container');
 					bodyClass.classList.remove('ast-two-container' , 'ast-page-builder-template' , 'ast-separate-container');
-
 				}
 			});
 
+		var titleCheckbox = document.getElementById('site-post-title'),
+			titleBlock = document.querySelector('.editor-post-title__block');
 
+		titleCheckbox.addEventListener('change',function() {
 
-
-		var title_checkbox = document.getElementById('site-post-title');
-		var title_block = document.querySelector('.editor-post-title__block');
-
-		title_checkbox.addEventListener('change',function() {
-
-			if( title_checkbox.checked ){
-
-				title_block.style.opacity = '0.2';
-			}
-			else {
-				title_block.style.opacity = '1.0';
+			if( titleCheckbox.checked ){
+				titleBlock.style.opacity = '0.2';
+			} else {
+				titleBlock.style.opacity = '1.0';
 			}
 		});
 	}
