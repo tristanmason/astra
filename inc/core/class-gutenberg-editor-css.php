2228f6e69b96b58a2411671ebe5e038dd5ae6497
imnavanath <navanathb@bsf.io>
2021-03-30 16:13:50 +0530

    refactor: Added body class when site-layout is max-width

--- inc/core/class-gutenberg-editor-css.php
+++ inc/core/class-gutenberg-editor-css.php
@@ -309,15 +309,15 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 				),
 			);
 
-			if( astra_wp_version_compare( '5.7', '>=' ) ) {
-				$desktop_css[ '.edit-post-visual-editor' ] = array(
+			if ( astra_wp_version_compare( '5.7', '>=' ) ) {
+				$desktop_css['.edit-post-visual-editor']                            = array(
 					'padding' => '20px',
 				);
-				$desktop_css[ '.ast-page-builder-template .edit-post-visual-editor' ] = array(
+				$desktop_css['.ast-page-builder-template .edit-post-visual-editor'] = array(
 					'padding' => '10px',
 				);
-				$desktop_css[ '.edit-post-visual-editor .editor-styles-wrapper' ] = array(
-					'padding' => '0',
+				$desktop_css['.edit-post-visual-editor .editor-styles-wrapper']     = array(
+					'padding'    => '0',
 					'background' => 'transparent',
 				);
 			}
