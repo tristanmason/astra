ed819d378c5d8ee3551bc436332d6b190195a717
premanshup <premanshup@bsf.io>
2021-04-05 15:34:12 +0530

    style: Top padding and margin with sidebar padding managed for Content Boxed layout for CSS variables.

--- inc/dynamic-css/global-variables.php
+++ inc/dynamic-css/global-variables.php
@@ -21,14 +21,29 @@ add_filter( 'astra_dynamic_theme_css', 'astra_css_global_variables', 5 );
  * @since x.x.x
  */
 function astra_css_global_variables( $dynamic_css ) {
-	$global_css_variables = '
-	:root {
-		--primary-padding: 4em 0;
-		--article-padding: 5.34em 6.67em;
 
-		--body-background-color: #f5f5f5;
-		--content-background-color: #ffffff;
-	}';
+	switch ( astra_get_content_layout() ) {
+		case 'content-boxed-container':
+			$global_css_variables = '
+			:root {
+				--primary-padding: 4em 0;
+				--secondary-padding: 4em;
+				--article-padding: 5.34em 6.67em;
+
+
+				--body-background-color: #f5f5f5;
+				--content-background-color: #ffffff;
+			}
+			.ast-row {
+				margin-left: 0;
+    			margin-right: 0;
+			}';
+			break;
+
+		default:
+			$global_css_variables = '';
+			break;
+	}
 
 	$dynamic_css .= Astra_Enqueue_Scripts::trim_css( $global_css_variables );
 
