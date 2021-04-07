d1c83815500a69d59e2504526773c5b286688564
Rohit Patil <rohitp@bsf.io>
2021-04-07 13:27:08 +0530

    fix: update code structure

--- inc/core/builder/class-astra-builder-helper.php
+++ inc/core/builder/class-astra-builder-helper.php
@@ -800,9 +800,7 @@ final class Astra_Builder_Helper {
 	 */
 	public static function is_header_footer_builder_active() {
 
-		$astra_settings                             = get_option( ASTRA_THEME_SETTINGS );
-		$astra_settings['is-header-footer-builder'] = isset( $astra_settings['is-header-footer-builder'] ) ? (bool) $astra_settings['is-header-footer-builder'] : true;
-		return apply_filters( 'astra_is_header_footer_builder_active', $astra_settings['is-header-footer-builder'] );
+		return apply_filters( 'astra_is_header_footer_builder_active', (bool) astra_get_option( 'is-header-footer-builder', true ) );
 	}
 
 	/**
