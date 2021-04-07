a0103a560babba31d13cf216765ee001fd277529
Rohit Patil <rohitp@bsf.io>
2021-04-07 14:11:31 +0530

    fix: Updated code to check header builder active

--- inc/core/builder/class-astra-builder-helper.php
+++ inc/core/builder/class-astra-builder-helper.php
@@ -800,7 +800,9 @@ final class Astra_Builder_Helper {
 	 */
 	public static function is_header_footer_builder_active() {
 
-		return apply_filters( 'astra_is_header_footer_builder_active', (bool) astra_get_option( 'is-header-footer-builder', true ) );
+		$astra_settings           = get_option( ASTRA_THEME_SETTINGS );
+		$is_header_footer_builder = isset( $astra_settings['is-header-footer-builder'] ) ? (bool) $astra_settings['is-header-footer-builder'] : true;
+		return apply_filters( 'astra_is_header_footer_builder_active', $is_header_footer_builder );
 	}
 
 	/**
