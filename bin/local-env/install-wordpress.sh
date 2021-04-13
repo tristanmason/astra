97bba46a5dd3dee54b5fde4837a397e3aa4619c1
premanshup <premanshup@bsf.io>
2021-04-13 12:38:55 +0530

    fix: Allow root access.

--- bin/local-env/install-wordpress.sh
+++ bin/local-env/install-wordpress.sh
@@ -129,7 +129,7 @@ fi
 
 # Configure Theme check.
 echo -e $(status_message "anhskohbo/wp-cli-themecheck...")
-wp package install anhskohbo/wp-cli-themecheck
+wp package install anhskohbo/wp-cli-themecheck --allow-root
 
 echo -e $(status_message "themecheck --theme=astra...")
 wp plugin install theme-check --activate --allow-root
