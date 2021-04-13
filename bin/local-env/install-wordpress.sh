0353e207c1401350be41326a450edc6f0598ee7d
premanshup <premanshup@bsf.io>
2021-04-13 13:09:42 +0530

    giving permission

--- bin/local-env/install-wordpress.sh
+++ bin/local-env/install-wordpress.sh
@@ -129,7 +129,7 @@ fi
 
 # Configure Theme check.
 echo -e $(status_message "anhskohbo/wp-cli-themecheck...")
-wp package install anhskohbo/wp-cli-themecheck --allow-root
+php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
 
 echo -e $(status_message "themecheck --theme=astra...")
 wp plugin install theme-check --activate --allow-root
