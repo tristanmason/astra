20516cda68ab52fc05c41347bfcb33a70b173a82
imnavanath <navanathb@bsf.io>
2021-04-17 09:47:30 +0530

    refactor: Moved core controls for Related Posts into single config file

--- inc/customizer/configurations/layout/class-astra-blog-single-layout-configs.php
+++ inc/customizer/configurations/layout/class-astra-blog-single-layout-configs.php
@@ -100,6 +100,410 @@ if ( ! class_exists( 'Astra_Blog_Single_Layout_Configs' ) ) {
 					),
 				),
 
+				/**
+				 * Option: Related Posts setting.
+				 */
+				array(
+					'name'     => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+					'default'  => astra_get_option( 'enable-related-posts' ),
+					'type'     => 'control',
+					'control'  => 'ast-toggle-control',
+					'title'    => __( 'Enable Related Posts', 'astra' ),
+					'section'  => 'section-blog-single',
+					'priority' => 9,
+				),
+
+				/**
+				 * Option: Related Posts Query
+				 */
+				array(
+					'name'     => ASTRA_THEME_SETTINGS . '[related-posts-section-heading]',
+					'section'  => 'section-blog-single',
+					'type'     => 'control',
+					'control'  => 'ast-heading',
+					'title'    => __( 'Related Posts', 'astra' ),
+					'context'  => array(
+						Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'priority' => 11,
+				),
+
+				/**
+				 * Option: Related Posts Structure
+				 */
+				array(
+					'name'              => ASTRA_THEME_SETTINGS . '[related-posts-structure]',
+					'type'              => 'control',
+					'control'           => 'ast-sortable',
+					'sanitize_callback' => array( 'Astra_Customizer_Sanitizes', 'sanitize_multi_choices' ),
+					'section'           => 'section-blog-single',
+					'default'           => astra_get_option( 'related-posts-structure' ),
+					'priority'          => 12,
+					'context'           => array(
+						Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'             => __( 'Posts Structure', 'astra' ),
+					'choices'           => array(
+						'featured-image' => __( 'Featured Image', 'astra' ),
+						'title-meta'     => __( 'Title & Post Meta', 'astra' ),
+					),
+					'divider'     => array( 'ast_class' => 'ast-top-divider' ),
+				),
+
+				array(
+					'name'              => ASTRA_THEME_SETTINGS . '[related-posts-meta-structure]',
+					'type'              => 'control',
+					'control'           => 'ast-sortable',
+					'sanitize_callback' => array( 'Astra_Customizer_Sanitizes', 'sanitize_multi_choices' ),
+					'default'           => astra_get_option( 'related-posts-meta-structure' ),
+					'context'           => array(
+						Astra_Builder_Helper::$general_tab_config,
+						'relation' => 'AND',
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[related-posts-structure]',
+							'operator' => 'contains',
+							'value'    => 'title-meta',
+						),
+					),
+					'section'           => 'section-blog-single',
+					'priority'          => 12,
+					'title'             => __( 'Meta', 'astra' ),
+					'choices'           => array(
+						'comments' => __( 'Comments', 'astra' ),
+						'category' => __( 'Category', 'astra' ),
+						'author'   => __( 'Author', 'astra' ),
+						'date'     => __( 'Publish Date', 'astra' ),
+						'tag'      => __( 'Tag', 'astra' ),
+					),
+				),
+
+				/**
+				 * Option: Enable excerpt for Related Posts.
+				 */
+				array(
+					'name'     => ASTRA_THEME_SETTINGS . '[enable-related-posts-excerpt]',
+					'default'  => astra_get_option( 'enable-related-posts-excerpt' ),
+					'type'     => 'control',
+					'control'  => 'ast-toggle-control',
+					'title'    => __( 'Enable Post Excerpt', 'astra' ),
+					'section'  => 'section-blog-single',
+					'priority' => 12,
+					'context'           => array(
+						Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+				),
+
+				/**
+				 * Option: Excerpt word count for Related Posts
+				 */
+				array(
+					'name'        => ASTRA_THEME_SETTINGS . '[related-posts-excerpt-count]',
+					'default'     => astra_get_option( 'related-posts-excerpt-count' ),
+					'type'        => 'control',
+					'control'     => 'ast-slider',
+					'context'     => array(
+						Astra_Builder_Helper::$general_tab_config,
+						'relation' => 'AND',
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts-excerpt]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'section'     => 'section-blog-single',
+					'title'       => __( 'Excerpt Word Count', 'astra' ),
+					'priority'    => 12,
+					'input_attrs' => array(
+						'min'  => 0,
+						'step' => 1,
+						'max'  => 60,
+					),
+				),
+
+				/**
+				 * Option: No. of Related Posts
+				 */
+				array(
+					'name'        => ASTRA_THEME_SETTINGS . '[related-posts-total-count]',
+					'default'     => astra_get_option( 'related-posts-total-count' ),
+					'type'        => 'control',
+					'control'     => 'ast-slider',
+					'context'     => array(
+						Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'section'     => 'section-blog-single',
+					'title'       => __( 'Total Number of Related Posts', 'astra' ),
+					'priority'    => 11,
+					'input_attrs' => array(
+						'min'  => -1,
+						'step' => 1,
+						'max'  => 20,
+					),
+					'divider'     => array( 'ast_class' => 'ast-bottom-divider' ),
+				),
+
+				/**
+				 * Option: Related Posts Columns
+				 */
+				array(
+					'name'       => ASTRA_THEME_SETTINGS . '[related-posts-grid]',
+					'type'       => 'control',
+					'control'    => 'ast-selector',
+					'section'    => 'section-blog-single',
+					'default'    => astra_get_option( 'related-posts-grid' ),
+					'priority'   => 11,
+					'context'    => array(
+						Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'      => __( 'Grid Column Layout', 'astra' ),
+					'choices'    => array(
+						'1' => __( '1', 'astra' ),
+						'2' => __( '2', 'astra' ),
+						'3' => __( '3', 'astra' ),
+						'4' => __( '4', 'astra' ),
+					),
+					'responsive' => false,
+					'renderAs'   => 'text',
+					'divider'    => array( 'ast_class' => 'ast-bottom-divider' ),
+				),
+
+				/**
+				 * Option: Related Posts Query group setting
+				 */
+				array(
+					'name'      => ASTRA_THEME_SETTINGS . '[related-posts-query-group]',
+					'default'   => astra_get_option( 'related-posts-query-group' ),
+					'type'      => 'control',
+					'transport' => 'postMessage',
+					'control'   => 'ast-settings-group',
+					'context'   => array(
+						Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'     => __( 'Posts Query', 'astra' ),
+					'section'   => 'section-blog-single',
+					'priority'  => 11,
+				),
+
+				/**
+				 * Option: Related Posts based on.
+				 */
+				array(
+					'name'       => 'related-posts-based-on',
+					'default'    => astra_get_option( 'related-posts-based-on' ),
+					'type'       => 'sub-control',
+					'transport'  => 'postMessage',
+					'parent'     => ASTRA_THEME_SETTINGS . '[related-posts-query-group]',
+					'section'    => 'section-blog-single',
+					'priority'   => 1,
+					'control'    => 'ast-selector',
+					'title'      => __( 'Related Posts by', 'astra' ),
+					'choices'    => array(
+						'categories' => __( 'Categories', 'astra' ),
+						'tags'       => __( 'Tags', 'astra' ),
+					),
+					'responsive' => false,
+					'renderAs'   => 'text',
+				),
+
+				/**
+				 * Option: Display Post Structure
+				 */
+				array(
+					'name'      => 'related-posts-order-by',
+					'default'   => astra_get_option( 'related-posts-order-by' ),
+					'parent'    => ASTRA_THEME_SETTINGS . '[related-posts-query-group]',
+					'section'   => 'section-blog-single',
+					'type'      => 'sub-control',
+					'priority'  => 2,
+					'transport' => 'postMessage',
+					'title'     => __( 'Order by', 'astra' ),
+					'control'   => 'ast-select',
+					'choices'   => array(
+						'date'          => __( 'Date', 'astra' ),
+						'title'         => __( 'Title', 'astra' ),
+						'post-order'    => __( 'Post Order', 'astra' ),
+						'random'        => __( 'Random', 'astra' ),
+						'comment-count' => __( 'Comment Counts', 'astra' ),
+					),
+				),
+
+				/**
+				 * Option: Display Post Structure
+				 */
+				array(
+					'name'       => 'related-posts-order',
+					'parent'     => ASTRA_THEME_SETTINGS . '[related-posts-query-group]',
+					'section'    => 'section-blog-single',
+					'type'       => 'sub-control',
+					'transport'  => 'postMessage',
+					'title'      => __( 'Order', 'astra' ),
+					'default'    => astra_get_option( 'related-posts-order' ),
+					'control'    => 'ast-selector',
+					'priority'   => 3,
+					'choices'    => array(
+						'asc'  => __( 'Ascending', 'astra' ),
+						'desc' => __( 'Descending', 'astra' ),
+					),
+					'responsive' => false,
+					'renderAs'   => 'text',
+				),
+
+				/**
+				 * Option: Related Posts colors setting group
+				 */
+				array(
+					'name'      => ASTRA_THEME_SETTINGS . '[related-posts-colors-group]',
+					'default'   => astra_get_option( 'related-posts-colors-group' ),
+					'type'      => 'control',
+					'transport' => 'postMessage',
+					'control'   => 'ast-settings-group',
+					'context'   => array(
+						true === Astra_Builder_Helper::$is_header_footer_builder_active ?
+						Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'     => __( 'Content Colors', 'astra' ),
+					'section'   => 'section-blog-single',
+					'priority'  => 15,
+				),
+
+				/**
+				 * Option: Related Posts title typography setting group
+				 */
+				array(
+					'name'      => ASTRA_THEME_SETTINGS . '[related-posts-section-title-typography-group]',
+					'type'      => 'control',
+					'priority'  => 16,
+					'control'   => 'ast-settings-group',
+					'context'   => array(
+						true === Astra_Builder_Helper::$is_header_footer_builder_active ?
+						Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'     => __( 'Section Title Font', 'astra' ),
+					'section'   => 'section-blog-single',
+					'transport' => 'postMessage',
+				),
+
+				/**
+				 * Option: Related Posts title typography setting group
+				 */
+				array(
+					'name'      => ASTRA_THEME_SETTINGS . '[related-posts-title-typography-group]',
+					'type'      => 'control',
+					'priority'  => 17,
+					'control'   => 'ast-settings-group',
+					'context'   => array(
+						true === Astra_Builder_Helper::$is_header_footer_builder_active ?
+						Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'     => __( 'Post Title Font', 'astra' ),
+					'section'   => 'section-blog-single',
+					'transport' => 'postMessage',
+				),
+
+				/**
+				 * Option: Related Posts meta typography setting group
+				 */
+				array(
+					'name'      => ASTRA_THEME_SETTINGS . '[related-posts-meta-typography-group]',
+					'type'      => 'control',
+					'priority'  => 19,
+					'control'   => 'ast-settings-group',
+					'context'   => array(
+						true === Astra_Builder_Helper::$is_header_footer_builder_active ?
+						Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config,
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'     => __( 'Meta Font', 'astra' ),
+					'section'   => 'section-blog-single',
+					'transport' => 'postMessage',
+				),
+
+				/**
+				 * Option: Related Posts content typography setting group
+				 */
+				array(
+					'name'      => ASTRA_THEME_SETTINGS . '[related-posts-content-typography-group]',
+					'type'      => 'control',
+					'priority'  => 21,
+					'control'   => 'ast-settings-group',
+					'context'   => array(
+						true === Astra_Builder_Helper::$is_header_footer_builder_active ?
+						Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config,
+						'relation' => 'AND',
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts]',
+							'operator' => '==',
+							'value'    => true,
+						),
+						array(
+							'setting'  => ASTRA_THEME_SETTINGS . '[enable-related-posts-excerpt]',
+							'operator' => '==',
+							'value'    => true,
+						),
+					),
+					'title'     => __( 'Content Font', 'astra' ),
+					'section'   => 'section-blog-single',
+					'transport' => 'postMessage',
+				),
 			);
 
 			if ( ! defined( 'ASTRA_EXT_VER' ) ) {
