2e87a228bdb10357819383dc3447fc6387a80453
Nikhil Chavan <email@nikhilchavan.com>
2021-04-30 12:47:52 +0530

    Add more constants

--- tests/php/static-analysis-stubs/constants.php
+++ tests/php/static-analysis-stubs/constants.php
@@ -2,7 +2,7 @@
 
 define( 'ASTRA_THEME_DIR', __DIR__ );
 define( 'ASTRA_THEME_URI', '' );
-define( 'SCRIPT_DEBUG', false );
+define( 'SCRIPT_DEBUG', true );
 define( 'WP_PLUGIN_DIR', '' );
 define( 'ASTRA_HEADER_SITE_IDENTITY_URI', '' );
 define( 'ASTRA_HEADER_SEARCH_URI', '' );
