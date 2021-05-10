8e0b850513b1fb71f9b7ebd6325327e960875022
sagarb <sagarb@bsf.io>
2021-05-10 12:01:12 +0530

    removed unused off-canvas css

--- inc/builder/type/header/off-canvas/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/off-canvas/dynamic-css/dynamic.css.php
@@ -458,10 +458,8 @@ function astra_dropdown_type_static_css() {
 	#ast-desktop-header .ast-desktop-header-content,
 	.ast-mobile-header-content .ast-search-icon,
 	.ast-desktop-header-content .ast-search-icon,
-	.ast-off-canvas-active .ast-main-header-nav-open.ast-popup-nav-open .ast-mobile-header-wrap .ast-mobile-header-content,
 	.ast-mobile-header-wrap .ast-mobile-header-content,
 	.ast-main-header-nav-open.ast-popup-nav-open .ast-mobile-header-wrap .ast-mobile-header-content,
-	.ast-off-canvas-active .ast-main-header-nav-open.ast-popup-nav-open .ast-desktop-header-content,
 	.ast-main-header-nav-open.ast-popup-nav-open .ast-desktop-header-content {
 	  display: none;
 	}
